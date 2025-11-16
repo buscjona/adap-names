b3d4b909212df11fcc83b007f7b31f35ce148c60
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2025-11-16 18:47:33 +0100

    Implemented StringName.ts using StringArrayName.ts as a template and looking at the required unit tests

--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -16,50 +16,114 @@ export class StringName implements Name {
         } else {
             this.delimiter = DEFAULT_DELIMITER;
         }
+        this.noComponents = this.name.split(this.delimiter).length;
     }
 
+    /**
+    Can be supplied with a choosen delimiter, if it isn't provided the default delimiter is used.
+    Combines the components of this.name using the choosen/default delimiter.
+    **/
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        let result: string = "";
+        for (let i = 0; i < this.noComponents; i++) {
+            let comp = this.getComponent(i);
+            result += comp;
+            // No delimiter after last comp
+            if (i < this.noComponents -1) {
+                result += delimiter;
+            }
+        }
+        return result;
     }
 
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        let result: string = "";
+        for (let char of this.name) {
+            if (char === this.delimiter || char === ESCAPE_CHARACTER) {
+                    result += ESCAPE_CHARACTER;
+            }
+            result += char;
+        }
+        return result;
     }
 
+    // @methodtype get-method
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
+    // @methodtype boolean-query-method
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents === 0;
     }
 
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
+    // @methodtype get-method
     public getComponent(x: number): string {
-        throw new Error("needs implementation or deletion");
+        this.assertIndexInRange(x);
+        return this.name.split(this.delimiter)[x];
     }
 
+    // @methodtype set-method
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.assertIndexInRange(n);
+        let temp: string[] = this.name.split(this.delimiter);
+        temp[n] = c;
+        this.name = temp.join(this.delimiter);
     }
 
+    // @methodtype command-method
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        // insert allows i === length
+        if (n < 0 || n > this.noComponents) {
+            throw new Error(`IndexError: Index Out of Range`);
+        }
+        let temp: string[] = this.name.split(this.delimiter);
+        temp.splice(n, 0, c);
+        this.name = temp.join(this.delimiter);
+        // We added an element so noComponents goes up by one
+        this.noComponents += 1;
     }
 
+    // @methodtype command-method
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        let temp: string[] = this.name.split(this.delimiter);
+        temp.push(c);
+        this.name = temp.join(this.delimiter);
+        this.noComponents += 1;
     }
 
+    // @methodtype command-method
     public remove(n: number): void {
-        throw new Error("needs implementation or deletion");
+        this.assertIndexInRange(n);
+        let temp: string[] = this.name.split(this.delimiter);
+        temp.splice(n, 1);
+        this.name = temp.join(this.delimiter);
+        this.noComponents -= 1; 
     }
 
+    // @methodtype command-method
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        let temp: string[] = this.name.split(this.delimiter);
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            temp.push(other.getComponent(i));
+        }
+        this.name = temp.join(this.delimiter);
+        // we need temp.lenth, since we donÄt know beforehand how many components the other Name object had
+        this.noComponents = temp.length;
+    }
+
+    // @methodtype assertion-method
+    private assertIndexInRange(i: number): void {
+        if (i < 0 || i >= this.noComponents) {
+            throw new Error(`IndexError: Index Out of Range`);
+        }
     }
 
 }
